e6f6b22c10dc9857bae7d80ed4d3083ac603a426
Alberto Chillardinho <chill-institute@proton.me>
2026-03-16 00:16:22 +0300

    fix(web): stop forwarding direct download routes

--- functions/download/[[path]].js
+++
@@ -1,9 +0,0 @@
-import { resolveHostedAPIBaseURL } from "../_lib/api-origin.js";
-
-export function onRequest({ request }) {
-  const url = new URL(request.url);
-  const apiOrigin = resolveHostedAPIBaseURL(url.hostname);
-  const target = new URL(`${url.pathname}${url.search}`, apiOrigin);
-
-  return Response.redirect(target.toString(), 308);
-}
